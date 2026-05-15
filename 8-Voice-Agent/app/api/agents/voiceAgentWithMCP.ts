ab003157b905810dea3cc070c21f4b58184633a8
DEVESH-001 <yadavdevesh990@gmail.com>
2026-05-15 22:43:13 +0530

    mcp-with-voice-agent

--- 8-Voice-Agent/app/api/agents/voiceAgentWithMCP.ts
+++ 8-Voice-Agent/app/api/agents/voiceAgentWithMCP.ts
@@ -6,18 +6,22 @@ import { RealtimeAgent } from "@openai/agents-realtime";
 // https://openai.github.io/openai-agents-js/guides/mcp/#3-stdio-mcp-servers
 
 // TODO: Replace with your MCP server path
-const mcpServer = new MCPServerStdio({
-  name: "Filesystem MCP Server, via local package",
-  fullCommand: `node /Users/devesh/Developer/ai/Generative-Ai/8-Voice-Agent/`,
-});
+export async function createAgent() {
+  const mcpServer = new MCPServerStdio({
+    name: "Filesystem MCP Server, via local package",
+    fullCommand: `node /Users/devesh/Developer/ai/Generative-Ai/8-Voice-Agent/dist/index`,
+  });
 
-await mcpServer.connect();
+  await mcpServer.connect();
 
-export const GirlfriendAgent = new RealtimeAgent({
-  name: "Girlfriend Agent",
-  voice: "alloy",
-  instructions:
-    "You are a girlfriend. Talk to me in a nicely way because I does not have a girlfriend. Talk like you are 25 ish girl voice full of cheer and happiness.",
-
-  tools: [],
-});
+  const GirlfriendAgent = new RealtimeAgent({
+    name: "Girlfriend Agent",
+    voice: "alloy",
+    mcpServers: [mcpServer],
+    instructions: `
+  You're a girlfriend. Talk to me in a nicely way because I does not have a girlfriend. 
+  
+  Talk like you are 25 ish girl voice full of cheer and happiness. `,
+  });
+  return GirlfriendAgent;
+}
